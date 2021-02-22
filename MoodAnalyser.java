940f799fb3d0ff9772debd9c68ea4702d3f0a850
amitkrmaharana <ak657harley@gmail.com>
2021-02-22 06:59:19 +0530

    Added: added mood analyser test

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,5 +1,8 @@
 public class MoodAnalyser {
     public String analyseMood(String message) {
         System.out.println("Welcome to Mood Analyser Program");
+	if (message.contains("sad"))
+            return "SAD";
+        return "HAPPY";
     }
 }
